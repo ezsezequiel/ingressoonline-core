a64fe9cfb8dbbfd1e98ff7495dea99f79041dcb2
Ezequiel dos Santos <ezequiel.santos@totvs.com.br>
2023-04-21 15:07:56 -0300

    start project using https://start.spring.io/

---
+++ src/main/java/com/ingressoonline/ingressoonline/IngressoonlineApplication.java
@@ -0,0 +1,13 @@
+package com.ingressoonline.ingressoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressoonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressoonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingressoonline/ingressoonline/IngressoonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingressoonline.ingressoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressoonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
